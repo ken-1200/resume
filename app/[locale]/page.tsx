3ec8d6a8d9f60b50e4e09ba203e9f2445814c56e
ken-1200 <machidahouse.lab@gmail.com>
2025-04-14 02:24:38 +0900

    feat: add routing and localization support with not found handling and sitemap generation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -12,7 +12,11 @@ export async function generateMetadata({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: "metadata" });
+  const typedLocale = locale as "ja" | "en";
+  const t = await getTranslations({
+    locale: typedLocale,
+    namespace: "metadata",
+  });
 
   return {
     title: t("title"),
@@ -23,7 +27,7 @@ export async function generateMetadata({
 export default async function HomePage({
   params,
 }: {
-  params: Promise<{ locale: string }>;
+  params: Promise<{ locale: "ja" | "en" }>;
 }) {
   const { locale } = await params;
 
---
+++ app/sitemap.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from "next";
+import { Locale } from "next-intl";
+import { host } from "@/config";
+import { routing } from "@/i18n/routing";
+import { getPathname } from "@/i18n/navigation";
+
+type Href = Parameters<typeof getPathname>[0]["href"];
+
+function getUrl(href: Href, locale: Locale) {
+  const pathname = getPathname({ locale, href });
+  return host + pathname;
+}
+
+function getEntries(href: Href) {
+  return routing.locales.map((locale) => ({
+    url: getUrl(href, locale),
+    alternates: {
+      languages: Object.fromEntries(
+        routing.locales.map((cur) => [cur, getUrl(href, cur)])
+      ),
+    },
+  }));
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [...getEntries("/")];
+}
