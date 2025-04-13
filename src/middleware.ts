722bb164bb9c74018be9f94c6fe053d30eb16ddb
ken-1200 <machidahouse.lab@gmail.com>
2025-04-14 04:07:03 +0900

    fix: refactor middleware to use routing import and clean up locale configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,7 @@
 import createMiddleware from "next-intl/middleware";
-// import { routing } from "@/src/i18n/routing";
+import { routing } from "@/src/i18n/routing";
 
-export default createMiddleware({
-  // List of all locales that are supported
-  locales: ["en", "ja"],
-
-  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: "ja",
-});
+export default createMiddleware(routing);
 
 export const config = {
   // Match all pathnames except for
