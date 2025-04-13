6a2b970184739c66adcd776b49cb3859f65a12af
ken-1200 <machidahouse.lab@gmail.com>
2025-04-14 00:18:37 +0900

    feat: add initial internationalization configuration for supported locales

---
+++ next-intl.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // List of all locales that are supported
+  locales: ["en", "ja"],
+
+  // Default locale
+  defaultLocale: "ja",
+
+  // Optional: Define paths that should not be internationalized
+  // pathnames: {
+  //   '/api': {
+  //     locale: false
+  //   }
+  // }
+};
